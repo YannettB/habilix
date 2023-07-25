37f17acd2dbe7225ea76c59929e88e751e8f8bdb
Yannett <rincondenane@gmail.com>
2023-07-25 00:20:48 -0400

    usuarios

--- src/app/components/knowledge-management/knowledge-management.component.ts
+++ src/app/components/knowledge-management/knowledge-management.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { Observable, Subject, Subscription, catchError, tap } from 'rxjs';
-import { SkillDataNode, SkillPlanNode } from 'src/app/models/skill-plan.model';import { Usuario } from 'src/app/models/skill-user.model';
+import { SkillDataNode, SkillPlanNode } from 'src/app/models/skill-plan.model';import { Conocimiento, Usuario } from 'src/app/models/skill-user.model';
 ;
 import { FirebaseDataService } from 'src/app/services/firebase-data.service';
 
@@ -15,11 +15,20 @@ import { FirebaseDataService } from 'src/app/services/firebase-data.service';
 export class KnowledgeManagementComponent implements OnInit {
   jsonForm: FormGroup;
   usuarioForm: FormGroup;
-  usuario: Usuario = new Usuario();
+  conocimientoForm: FormGroup;
+
   jsonData: SkillPlanNode = new SkillPlanNode;
+
   displayedColumns: string[] = ['id', 'lenguaje', 'acciones'];
+  displayedColumnsUsuario: string[] = ['usuario', 'acciones'];
+  displayedColumnsConocimiento: string[] = ['tec_id', 'puntuacion'];
+  
+  dataSourceConocimiento: MatTableDataSource<Conocimiento> = new MatTableDataSource<Conocimiento>();
   dataSource: any[] = [];
+  dataSourceUsuario: MatTableDataSource<Usuario> = new MatTableDataSource<Usuario>();
+
   @ViewChild(MatTable) tablaSkillPlan!: MatTable<SkillPlanNode>;
+  detalleConocimiento: Usuario | undefined;
 
   constructor(private fireBaseDataService: FirebaseDataService, private formBuilder: FormBuilder, private router: Router) {
     this.jsonForm = this.formBuilder.group({
@@ -29,37 +38,61 @@ export class KnowledgeManagementComponent implements OnInit {
       nombre: ['', Validators.required],
       conocimientos: this.formBuilder.array([]) // Inicializamos como un FormArray vacío
     });
+    this.conocimientoForm = this.formBuilder.group({
+      tec_id: ['', Validators.required],
+      puntuacion: ['', Validators.required] // Inicializamos como un FormArray vacío
+    });
   }
 
   ngOnInit(): void {
     this.cargarDatosPlan();
   }
 
-  // Obtener el control del FormArray para conocimientos
-  get conocimientosArray(): FormArray {
+  get conocimientosFormArray() {
     return this.usuarioForm.get('conocimientos') as FormArray;
   }
 
-  // Agregar un nuevo conocimiento al FormArray de conocimientos
-  agregarConocimiento(): void {
-    this.conocimientosArray.push(this.formBuilder.group({
+  agregarUsuario() {
+    this.dataSourceUsuario.data.push({
+      nombre: this.usuarioForm.get('nombre')?.value,
+      conocimientos: []
+    });
+    this.dataSourceUsuario._updateChangeSubscription(); // Actualizar datos de la tabla
+    this.usuarioForm.reset();
+  }
+
+  verDetalle(usuario: Usuario) {
+    this.detalleConocimiento = usuario;
+    this.dataSourceConocimiento.data = usuario.conocimientos;
+  }
+
+  agregarConocimiento() {
+    //const conocimientoForm = this.usuarioForm.get('conocimientos') as FormArray;
+    /*
+    conocimientoForm.push(this.formBuilder.group({ 
       tec_id: ['', Validators.required],
-      puntuacion: [0, Validators.required]
-    }));
+      puntuacion: ['', Validators.required]
+  }));
+  */
+  if (this.detalleConocimiento) {
+    this.detalleConocimiento.conocimientos.push({
+      tec_id: this.conocimientoForm.get('tec_id')?.value,
+      puntuacion: this.conocimientoForm.get('puntuacion')?.value
+    });
+    this.dataSourceConocimiento.data = this.detalleConocimiento.conocimientos;
   }
+  this.dataSourceConocimiento._updateChangeSubscription(); // Actualizar datos de la tabla
+  this.conocimientoForm.reset();
+}
 
-  // Remover un conocimiento del FormArray de conocimientos
-  removerConocimiento(index: number): void {
-    this.conocimientosArray.removeAt(index);
+onSubmitUser(): void {
+  if (this.usuarioForm.valid) {
+    const nuevoUsuario: Usuario = this.usuarioForm.value;
+    // Aquí puedes hacer lo que necesites con el nuevo usuario, como guardarlo en la base de datos
+    console.log(nuevoUsuario);
   }
+}
 
-  onSubmitUser(): void {
-    if (this.usuarioForm.valid) {
-      const nuevoUsuario: Usuario = this.usuarioForm.value;
-      console.log(nuevoUsuario);
-      // Aquí puedes hacer lo que necesites con el nuevo usuario, como enviarlo a Firebase para guardarlo
-    }
-  }
 
   onSubmit(): void {
     if (this.jsonForm.valid) {
