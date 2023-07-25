42f1364a89da72488b1687632253ed44f773e9d4
Yannett <rincondenane@gmail.com>
2023-07-25 13:30:51 -0400

    ajuste recursivo para tecnologias

--- src/app/services/knowledge.service.ts
+++ src/app/services/knowledge.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FirebaseDataService } from './firebase-data.service';
-import { Observable, map } from 'rxjs';
+import { Observable, map, switchMap } from 'rxjs';
 import { SkillTec } from '../models/skill-tec.model';
 import { SkillData } from '../models/skill-data.model';
 import { Conocimiento, Usuario } from '../models/skill-user.model';
@@ -14,36 +14,50 @@ export class KnowledgeService {
   constructor(private firebasedataService: FirebaseDataService) { }
 
   getPlanPuntajes(planId: string): Observable<any> {
-    // Obtiene el plan de trabajo y el equipo
+    // Obtiene el plan de trabajo
     let planObservable = this.firebasedataService.getDocumentbyAttr("PlanDeTrabajo", "id", planId);
-    let equipoObservable = this.firebasedataService.getCollection("Usuarios");
-
+  
     // Combina los dos observables y calcula la media del conocimiento
     return planObservable.pipe(
-      map((plan: SkillPlanNode) => {
-        equipoObservable.subscribe(equipo => {
-          // Filtra el equipo para obtener solo los usuarios asociados a este plan
-          let usuariosAsociados = equipo.filter(usuario => plan.usuarios.includes(usuario.id));
-
-          // Asumimos que 'plan' y 'usuariosAsociados' son arrays y seguimos la lógica de la respuesta anterior
-          plan.ruta.forEach((tec:SkillDataNode) => {
-            
-            let puntuaciones = usuariosAsociados.flatMap((usuario: Usuario) => {
-              return usuario.conocimientos
-                .filter((conocimiento: Conocimiento) => conocimiento.tec_id === tec.tec.id)
-                .map(conocimiento => conocimiento.puntuacion);
-            });
-            
-            let suma = puntuaciones.reduce((a, b) => Number(a) + Number(b), 0);
-            let media = suma / puntuaciones.length;
-            
-            tec.puntuacion = media;
-            
-          });
-        });
-
-        return plan;
+      switchMap((plan: SkillPlanNode) => {
+        // Obtiene el equipo
+        let equipoObservable = this.firebasedataService.getCollection("Usuarios");
+  
+        // Mapea el equipo a los usuarios asociados y calcula la media del conocimiento
+        return equipoObservable.pipe(
+          map(equipo => {
+            // Filtra el equipo para obtener solo los usuarios asociados a este plan
+            let usuariosAsociados = equipo.filter(usuario => plan.usuarios.includes(usuario.id));
+  
+            // Asumimos que 'plan' y 'usuariosAsociados' son arrays y seguimos la lógica de la respuesta anterior
+            this.obtenerMediaPuntaje(plan.ruta, usuariosAsociados);
+  
+            return plan;
+          })
+        );
       })
     );
+  }
+  
+
+  private obtenerMediaPuntaje(ruta: SkillDataNode[], usuariosAsociados: any[]) {
+    ruta.forEach((tec: SkillDataNode) => {
+
+      let puntuaciones = usuariosAsociados.flatMap((usuario: Usuario) => {
+        return usuario.conocimientos
+          .filter((conocimiento: Conocimiento) => conocimiento.tec_id === tec.tec.id)
+          .map(conocimiento => conocimiento.puntuacion);
+      });
+
+      let suma = puntuaciones.reduce((a, b) => Number(a) + Number(b), 0);
+      let media = suma / puntuaciones.length;
+
+      tec.puntuacion = Number(media.toFixed(1));
+
+      if(tec.childs && tec.childs.length > 0) {
+        this.obtenerMediaPuntaje(tec.childs, usuariosAsociados);
+      }
+
+    });
   }
 }
